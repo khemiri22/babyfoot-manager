4dbc19e3cd03108c9114dfb823b472da2ca9c539
khemiri22 <khe.amine22@gmail.com>
2025-11-19 22:28:49 +0000

    update db config

--- utilities/db.js
+++ utilities/db.js
@@ -6,7 +6,8 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: parseInt(process.env.DB_PORT)
+    port: parseInt(process.env.DB_PORT),
+    ssl: true
 });
 
 // Export a query function
